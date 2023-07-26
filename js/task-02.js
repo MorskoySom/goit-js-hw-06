6a41f46034c22a04f8e7a5192ccc68594930e34a
MorskoySom <Somovvitvit@gmail.com>
2023-07-26 14:27:11 +0300

    second

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,25 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listFinal = document.querySelector(`#ingredients`);
+const list = [];
+
+ingredients.map(elem => {
+  const li = document.createElement(`li`);
+  li.textContent = elem;
+  li.classList.add(`item`);
+  list.push(li);
+});
+console.log(list);
+
+listFinal.append(...list);
+
+
+// const li = document.createElement(`li`);
+// li.textContent = ingredients[0];
+// li.classList.add(`item`);
+// console.log(li);
+
+// const li = `<li class="item">`ingredients[0]`</li>`;
+// console.log(li);
