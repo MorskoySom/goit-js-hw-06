a8e963c9a17115668a2bb5f1d8bf04f542469e55
MorskoySom <Somovvitvit@gmail.com>
2023-07-27 19:13:44 +0300

    8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const elements = {
+    form: document.querySelector(`.login-form`),     
+}
+// console.log(elements);
+
+elements.form.addEventListener("submit", handlerCheckInput);
+
+function handlerCheckInput(evt) {
+    evt.preventDefault();
+    // console.dir(evt.currentTarget); 
+    const { email, password } = evt.currentTarget.elements;
+    // console.log(password.value);
+    // console.log(email.value);
+    if (!email.value || !password.value) {
+        alert(`All inputs must be used`);
+    } else {
+        const user = {  
+            email : email.value,
+            password : password.value
+        }
+        console.log(user);
+    }
+    evt.target.reset();
+}
 
