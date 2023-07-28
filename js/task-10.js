73e3e819e540caf999308ee4286d9974f7e4de19
MorskoySom <Somovvitvit@gmail.com>
2023-07-28 21:32:40 +0300

    all

--- js/task-10.js
+++ js/task-10.js
@@ -1,30 +1,37 @@
 const elements = {
   nmbr: document.querySelector('input'),
   createBox: document.querySelector('button[data-create]'),
-  destroyBox: document.querySelector('button[data-destroy]')  
-}
-console.log(elements.createBox);
-console.log(elements.destroyBox);
-console.log(elements.nmbr);
-
-elements.nmbr.addEventListener(`input`, handleAmount);
-function handleAmount(evt) {
-  // console.dir(evt.currentTarget.value);
-  let amount = Number(evt.currentTarget.value);
-  console.log(amount);
+  destroyBox: document.querySelector('button[data-destroy]'),
+  divsBox: document.querySelector('#boxes')
 }
 
-// elements.createBox.addEventListener(`click`, createBoxes);
+elements.createBox.addEventListener(`click`, createBoxes);
 
-// function createBoxes(amount) {
-//   // amount = Number(input.currentTarget.value);
-//   console.dir(elements.nmbr.input.value);
-// }
+function createBoxes(amount) { 
+  const totalDivs = [];
+  amount = elements.nmbr.valueAsNumber;
+  // console.dir(elements.nmbr.valueAsNumber);
+  if (amount >= 1 && amount <= 100) {
+    for (let i = 0; i < amount; i += 1) {
+      const eachDiv = document.createElement(`div`);
+      const eachDivColor = getRandomHexColor();
+      let step = 30;
+      step = 30 + (10 * i);
+      eachDiv.style.backgroundColor = eachDivColor;
+      eachDiv.style.width = `${step}px`;
+      eachDiv.style.height = `${step}px`;  
+      totalDivs.push(eachDiv);          
+    }       
+  }
+  console.log(totalDivs); 
+  elements.divsBox.append(...totalDivs);
+}
 
-  
+elements.destroyBox.addEventListener(`click`, destroyBoxes);
 
-
-
+function destroyBoxes() {
+  elements.divsBox.innerHTML = ``;
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
